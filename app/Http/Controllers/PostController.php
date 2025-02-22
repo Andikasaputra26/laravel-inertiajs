0a0c0269252c6b2c012d36ef54aa491fd1464683
Andika Saputra <acyandika2@gmail.com>
2025-02-22 11:36:31 +0800

    new template

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,10 +13,10 @@ class PostController extends Controller
      * Display a listing of the resource.
      */
 
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
 
     public function index()
     {
